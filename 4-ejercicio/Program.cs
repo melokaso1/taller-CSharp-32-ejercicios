34cd246845444adad7a6009dc94ef201a1795792
Alejandro escobar <107068494+melokaso1@users.noreply.github.com>
2026-03-23 20:15:42 -0500

    feat: :sparkles: contador de letras

---
+++ 4-ejercicio/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write(
+    @"ingresa un texto y el software analizara cuantas veces se repite un caracter
+
+ingresa el texto aca: "
+);
+
+string? texto = Console.ReadLine();
+
+texto = texto!.Replace(" ", string.Empty.ToLower());
+
+Dictionary<char, int> cant = new Dictionary<char, int>();
+
+foreach (char i in texto)
+{
+    if (cant.ContainsKey(i))
+        cant[i]++;
+    else
+        cant[i] = 1;
+}
+foreach (var j in cant)
+    Console.WriteLine($"Caracter = [{j.Key}] | Cantidad = [{j.Value}]");
